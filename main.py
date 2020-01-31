73f347b498280795c8950e3c812bcbd5f0df4b71
ShaoKui <shaokui@stumail.neu.edu.cn>
2020-01-31 13:06:34 +0800

    Simple Scatter Plot!

--- main.py
+++ main.py
@@ -18,6 +18,10 @@ def barchart():
 def baraxes():
     return flask.send_from_directory('static', 'baraxes.html')
 
+@app.route('/scatter')
+def scatter():
+    return flask.send_from_directory('static', 'scatter.html')
+
 @app.route('/static/<fname>')
 def sendfile(fname):
     return flask.send_from_directory('static', fname)

c4ce64a212c835f4b1120a414a7695ed6527c25c
ShaoKui <shaokui@stumail.neu.edu.cn>
2020-03-03 15:31:31 +0800

    Adding simplified examples for data-join.

--- main.py
+++ main.py
@@ -94,6 +94,10 @@ def line():
 def scatter_tutorial():
     return flask.send_from_directory('static', 'd3-tutorial/scatter.html')
 
+@app.route('/scatter-tutorial-simple')
+def scatter_tutorial_simple():
+    return flask.send_from_directory('static', 'd3-tutorial/scatter-simple.html')
+
 @app.route('/static/<fname>')
 def sendfile(fname):
     return flask.send_from_directory('static', fname)
